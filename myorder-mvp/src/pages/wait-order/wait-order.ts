590f6a9d690bf4a25960dde98579caa358513621
Caio Franco <caio_defreitas@hotmail.com>
2018-09-30 17:11:55 -0300

    Foi implementado a pagina de espera

--- myorder-mvp/src/pages/wait-order/wait-order.ts
+++ myorder-mvp/src/pages/wait-order/wait-order.ts
@@ -1,25 +1,65 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
-/**
- * Generated class for the WaitOrderPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
+import { UtilProvider } from "./../../providers/util/util";
+import { TimerComponent } from "./../../components/timer/timer";
+import { Component, ViewChild } from "@angular/core";
+import { IonicPage, NavController, NavParams } from "ionic-angular";
+import { Order } from "../../model/order";
+import { InAppBrowser, InAppBrowserOptions } from "@ionic-native/in-app-browser";
 
 @IonicPage()
 @Component({
-  selector: 'page-wait-order',
-  templateUrl: 'wait-order.html',
+  selector: "page-wait-order",
+  templateUrl: "wait-order.html",
 })
 export class WaitOrderPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  @ViewChild(TimerComponent) timer: TimerComponent;
+
+  public waitTime
+
+  public order: Order;
+
+  public Apps: any[] = [];
+  public optionsBrowser: InAppBrowserOptions = {
+    mediaPlaybackRequiresUserAction: "no",
+    hideurlbar: "no",
+    toolbar: "yes",
+    closebuttoncaption : "Close", //iOS only
+    disallowoverscroll : "no", //iOS only 
+    enableViewportScale : "no", //iOS only 
+    allowInlineMediaPlayback : "no",//iOS only 
+    presentationstyle : "pagesheet",//iOS only 
+  };
+
+  constructor(
+    private inAppBrowser: InAppBrowser,
+    public util: UtilProvider,
+    public navCtrl: NavController,
+    public navParams: NavParams) {
+      
+    this.order = navParams.get("order");
+    this.waitTime = this.order.time;
+    // this.waitTime = 300;
+    for (let i = 0; i < 50; i++) {
+      this.Apps.push(i);
+    }
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad WaitOrderPage');
+    this.timer.startTimer(this.waitTime);
   }
 
+  btnNewOrder() {
+    this.navCtrl.push("CategoriaPage");
+  }
+
+  btnRotation() {
+    this.util.showToast("VAI GIRAR!!!");
+  }
+
+  imageSelect(index) {
+    console.log("Clicou!!!", index);
+    const browser = this.inAppBrowser.create("https://www.uol.com.br/", "_self", this.optionsBrowser);
+
+    
+  }
 }
